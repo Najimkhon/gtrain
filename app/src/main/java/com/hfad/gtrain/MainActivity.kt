aee583594c0d3ada2be76b5f079d2d3103ccbf81
Najimkhon <qwerty1995.290@gmail.com>
2023-02-24 19:27:02 +0500

    navigate to logs is added

--- app/src/main/java/com/hfad/gtrain/MainActivity.kt
+++ app/src/main/java/com/hfad/gtrain/MainActivity.kt
@@ -3,17 +3,22 @@ package com.hfad.gtrain
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hfad.gtrain.databinding.ActivityMainBinding
+import com.hfad.gtrain.fragments.exerciseList.ExerciseListFragment
+import com.hfad.gtrain.fragments.logList.LogListFragment
+import com.hfad.gtrain.fragments.muscleGroupList.MuscleGroupListFragment
 import com.hfad.gtrain.viewmodels.MainViewmodel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val mainViewModel: MainViewmodel by viewModels()
-    
+    lateinit var bottomNav: BottomNavigationView
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,15 +28,6 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
-        val files = filesDir.listFiles()
-//        navController
-//            .addOnDestinationChangedListener{_, destination, _->
-//                when(destination.id){
-//                    R.id.exerciseDetailFragment -> binding.bottomNavigationView.visibility = View.GONE
-//                    else-> binding.bottomNavigationView.visibility = View.VISIBLE
-//                }
-//            }
+   }
 
-
-    }
 }
