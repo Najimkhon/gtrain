40fdb0ed95e501dd6b0b507ce459639306bf0c31
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2023-03-24 21:36:30 +0500

    reformatted MainActivity and Android Manifest

--- app/src/main/java/com/hfad/gtrain/MainActivity.kt
+++ app/src/main/java/com/hfad/gtrain/MainActivity.kt
@@ -1,28 +1,21 @@
 package com.hfad.gtrain
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hfad.gtrain.databinding.ActivityMainBinding
-import com.hfad.gtrain.fragments.exerciseList.ExerciseListFragment
-import com.hfad.gtrain.fragments.logList.LogListFragment
-import com.hfad.gtrain.fragments.muscleGroupList.MuscleGroupListFragment
 import com.hfad.gtrain.viewmodels.MainViewmodel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val mainViewModel: MainViewmodel by viewModels()
-    lateinit var bottomNav: BottomNavigationView
 
+    private val mainViewModel: MainViewmodel by viewModels()
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val splashScreen = installSplashScreen()
@@ -30,9 +23,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
-   }
-
+    }
 }
